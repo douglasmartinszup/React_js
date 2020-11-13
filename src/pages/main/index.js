dad8917a9e4e1a4362b27065865b7e2e6656fd5b
douglasmartinszup <douglas.martins@zup.com.br>
2020-11-13 19:50:06 -0300

    navegando entre paginas

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import api from "../../services/api";
+// eslint-disable-next-line no-unused-vars
 import styles from "./styles.css";
-
 
 export default class Main extends Component {
   /* Criar uma variável dentro de estado para armezanarmos 
@@ -9,6 +9,8 @@ export default class Main extends Component {
 
   state = {
     products: [],
+    productInfo: {},
+    page: 1
   };
   /* executar  uma ação logo que um componente é exibido em tela : componentDidMount  */
   componentDidMount() {
@@ -16,20 +18,43 @@ export default class Main extends Component {
   }
   /* função para carregar os produtos , utilizamos o async/await 
   para esperar a response ada api*/
-  loadProducts = async () => {
+  loadProducts = async (page = 1) => {
     /* criei uma const para receber a api
      que estamos solicitando com o api.get */
-    const response = await api.get("/products");
-    /*Criei um console.log  para conferir o retorno da api */
-    console.log(response.data.docs);
+    const response = await api.get(`/products?page=${page}`);
+    
+    /*Armazenando dados para rotas */
+    const {docs, ...productInfo} = response.data;
+
+    
     /* Armazenando a response dentro de products*/
-    this.setState({ products: response.data.docs });
+    this.setState({ products: docs, productInfo, page });
+  };
+
+  pervPage = () => {
+    
+    // eslint-disable-next-line no-unused-vars
+    const { page, productInfo } = this.state;
+    
+    if ( page === 1) return;
+
+    const pageNumber = page - 1;
+
+    this.loadProducts(pageNumber);
+  };
+
+  nextPage = () => {
+    const { page, productInfo } = this.state;
+
+    if (page === productInfo.pages) return;
+
+    const pageNumber = page + 1 ;
+
+    this.loadProducts(pageNumber);
   };
 
   render() {
-
-      const { products } = this.state;
-
+    const { products } = this.state;
 
     return (
       //listando os titulos dos produtos das response dentro de uma div
@@ -37,11 +62,14 @@ export default class Main extends Component {
         {products.map((product) => (
           <article key={product._id}>
             <strong>{product.title}</strong>
-        <p>{product.description}</p>
-        <a href="#">Acessar produtos</a>
+            <p>{product.description}</p>
+            <a href="#">Acessar produtos</a>
           </article>
-          
         ))}
+        <div className="actions">
+          <button onClick={this.prevPage}>Anterior</button>
+          <button onClick={this.nextPage}>Próxima</button>
+        </div>
       </div>
     );
   }
